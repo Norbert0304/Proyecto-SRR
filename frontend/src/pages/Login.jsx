5ec4f4b2738ce49eca179cb48ab8be39b84b774d
Juampa18 <juampaoc18@gmail.com>
2026-05-11 19:16:31 -0500

    Formulario login y validaciones

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,82 @@
+import { useForm } from "react-hook-form";
+
+function Login() {
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm();
+
+  const onSubmit = (data) => {
+    alert("Login correcto");
+    console.log(data);
+  };
+
+  return (
+    <div className="container mt-5">
+
+      <div className="card p-4 shadow">
+
+        <h2 className="mb-4 text-center">
+          Iniciar Sesión
+        </h2>
+
+        <form onSubmit={handleSubmit(onSubmit)}>
+
+          <div className="mb-3">
+            <label>Email</label>
+
+            <input
+              type="email"
+              className="form-control"
+              {...register("email", {
+                required: "El email es obligatorio"
+              })}
+            />
+
+            {errors.email && (
+              <p className="text-danger">
+                {errors.email.message}
+              </p>
+            )}
+          </div>
+
+          <div className="mb-3">
+            <label>Contraseña</label>
+
+            <input
+              type="password"
+              className="form-control"
+              {...register("password", {
+                required: "La contraseña es obligatoria",
+                minLength: {
+                  value: 6,
+                  message: "Mínimo 6 caracteres"
+                }
+              })}
+            />
+
+            {errors.password && (
+              <p className="text-danger">
+                {errors.password.message}
+              </p>
+            )}
+          </div>
+
+          <button
+            type="submit"
+            className="btn btn-primary w-100"
+          >
+            Ingresar
+          </button>
+
+        </form>
+
+      </div>
+
+    </div>
+  );
+}
+
+export default Login;
